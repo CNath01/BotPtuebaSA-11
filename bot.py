b90f59f5954cbfd54cacf27183d58fe169a1edc2
CNath01 <92951005+CNath01@users.noreply.github.com>
2024-11-30 12:26:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
-
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
